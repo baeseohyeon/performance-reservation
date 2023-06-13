9f843d1825a5d1fff520890129c94da25e2a5d13
baeseohyeon <formnarago@naver.com>
2023-06-13 10:56:33 +0900

    Initial commit

---
+++ src/main/java/com/numble/performancereservation/PerformanceReservationApplication.java
@@ -0,0 +1,13 @@
+package com.numble.performancereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numble/performancereservation/PerformanceReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.performancereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
