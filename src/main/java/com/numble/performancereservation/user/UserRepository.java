9d8007258909705e0e7e4a49e456cb33daa9682b
baeseohyeon <formnarago@naver.com>
2023-06-15 00:18:33 +0900

    FEAT: User, Authority 레포지토리 구현

---
+++ src/main/java/com/numble/performancereservation/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.numble.performancereservation.user;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+}
