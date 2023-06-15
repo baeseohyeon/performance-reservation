31ecd46bd5aa7b8b2341b3c9a4cfb00e2ff7e6cf
baeseohyeon <formnarago@naver.com>
2023-06-15 19:48:50 +0900

    FEAT: 공연장 엔티티에 유저 엔티티, 공연장 이용 가능 시간 필드 추가

--- src/main/java/com/numble/performancereservation/performance/Venue.java
+++ src/main/java/com/numble/performancereservation/performance/Venue.java
@@ -1,12 +1,17 @@
 package com.numble.performancereservation.performance;
 
 import com.numble.performancereservation.base.BaseEntity;
+import com.numble.performancereservation.user.User;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,10 +26,18 @@ public class Venue extends BaseEntity {
     private int capacity;
     @Enumerated
     private VenueType type;
+    @Embedded
+    private PossibleTime possibleTime;
+    @JoinColumn(name = "user_id")
+    @ManyToOne
+    private User user;
 
-    public Venue(String name, int capacity, VenueType type) {
+    @Builder
+    public Venue(User user, String name, int capacity, VenueType type, String possibleTime) {
+        this.user = user;
         this.name = name;
         this.capacity = capacity;
         this.type = type;
+        this.possibleTime = new PossibleTime(possibleTime);
     }
 }
