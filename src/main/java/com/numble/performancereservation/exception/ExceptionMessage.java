2985abaa237cff9f16bc9beed4230a8c24960696
baeseohyeon <formnarago@naver.com>
2023-06-15 19:41:15 +0900

    FEAT: 에러 메세지 추가

--- src/main/java/com/numble/performancereservation/exception/ExceptionMessage.java
+++ src/main/java/com/numble/performancereservation/exception/ExceptionMessage.java
@@ -5,4 +5,6 @@ public class ExceptionMessage {
     public static final String DUPLICATE_EMAIL = "email already exists";
     public static final String DUPLICATE_USERNAME = "username already exists";
     public static final String INVALID_USERID = "userId doesn't exists";
+    public static final String INVALID_USER = "user isn't producer";
+    public static final String INVALID_VENUE_USE_TIME = "use time of the venue must be at least 1 hour.";
 }
