1ac029dbe9056ea5f487791678d6ccdb5f50a119
baeseohyeon <formnarago@naver.com>
2023-06-17 23:48:37 +0900

    FEAT: 이미 예약된 좌석인지 검증하는 기능 구현

--- src/main/java/com/numble/performancereservation/performance/SeatRepository.java
+++ src/main/java/com/numble/performancereservation/performance/SeatRepository.java
@@ -1,8 +1,14 @@
 package com.numble.performancereservation.performance;
 
+import java.util.Collection;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
     long countByVenueId(Long venueId);
+
+    long countByIdIn(List<Long> ids);
+
+    boolean existsByIdInAndIsReserved(Collection<Long> ids, boolean isReserved);
 }
--- src/main/java/com/numble/performancereservation/performance/SeatService.java
+++ src/main/java/com/numble/performancereservation/performance/SeatService.java
@@ -1,5 +1,8 @@
 package com.numble.performancereservation.performance;
 
+import static com.numble.performancereservation.exception.ExceptionMessage.ALREADY_RESERVED_SEAT;
+import static com.numble.performancereservation.exception.ExceptionMessage.INVALID_SEAT_ID;
+
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +16,28 @@ public class SeatService {
     private final SeatRepository seatRepository;
     private final SeatJdbcRepository seatJdbcRepository;
 
-    public void saveAll(List<SeatDto> seats, Long venueId){
-        seatJdbcRepository.saveAll(seats,venueId);
+    public void saveAll(List<SeatDto> seats, Long venueId) {
+        seatJdbcRepository.saveAll(seats, venueId);
     }
 
     public long countByVenueId(Long venueId) {
         return seatRepository.countByVenueId(venueId);
+    }
+
+    public void validateSeatExistenceAndReservation(List<Long> seatIds) {
+        validateSeatAlreadyReserved(seatIds);
+        validateExistingSeat(seatIds);
+    }
+
+    private void validateSeatAlreadyReserved(List<Long> seatIds) {
+        if (seatRepository.existsByIdInAndIsReserved(seatIds, true)) {
+            throw new IllegalArgumentException(ALREADY_RESERVED_SEAT);
+        }
+    }
+
+    private void validateExistingSeat(List<Long> seatIds) {
+        if (seatRepository.countByIdIn(seatIds) != seatIds.size()) {
+            throw new IllegalArgumentException(INVALID_SEAT_ID);
+        }
     }
 }
