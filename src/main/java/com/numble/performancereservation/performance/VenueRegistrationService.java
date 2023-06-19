54aa1ba40797cc08c3891803f99ec8f81f4e20b4
baeseohyeon <formnarago@naver.com>
2023-06-19 17:40:39 +0900

    test

--- src/main/java/com/numble/performancereservation/performance/VenueRegistrationService.java
+++ src/main/java/com/numble/performancereservation/performance/VenueRegistrationService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 @Service
-public class VenueRegistrationService {
+public class VenueRegistrationService  {
 
     private final VenueService venueService;
     private final SeatService seatService;
