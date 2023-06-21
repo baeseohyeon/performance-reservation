c9c55a64b16cd62551c7ea6b3c4f5bc79671c4f5
baeseohyeon <formnarago@naver.com>
2023-06-21 11:41:02 +0900

    FEAT: 공연의 좌석 목록 조회 기능 구현

---
+++ src/main/java/com/numble/performancereservation/performance/PerformanceSeatQueryService.java
@@ -0,0 +1,20 @@
+package com.numble.performancereservation.performance;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class PerformanceSeatQueryService {
+
+    private final PerformanceQueryService performanceQueryService;
+    private final SeatService seatService;
+
+    public List<Seat> findAvailableSeatsByPerformanceId(Long performanceId) {
+        Performance performance = performanceQueryService.findById(performanceId);
+        return seatService.findSeatsByVenueId(performance.getVenue().getId());
+    }
+}
