2e0693ffdf83883a2fb83f6958f77cebfcfcc292
baeseohyeon <formnarago@naver.com>
2023-06-25 14:47:14 +0900

    FIX: 공연장 엔티티 유저 필드 지연 로딩으로 수정

--- src/main/java/com/numble/performancereservation/performance/Venue.java
+++ src/main/java/com/numble/performancereservation/performance/Venue.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -33,7 +34,7 @@ public class Venue extends BaseEntity {
     @Embedded
     private PossibleTime possibleTime;
     @JoinColumn(name = "user_id")
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     @Builder
