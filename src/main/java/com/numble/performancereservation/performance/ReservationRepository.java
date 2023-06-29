a3e0c47f6c4e0c0d78e757c32c4710a2150732f5
baeseohyeon <formnarago@naver.com>
2023-06-29 13:48:07 +0900

    FEAT: ReservationRepository 이미 예약된 좌석인지 확인하는 쿼리 구현

--- src/main/java/com/numble/performancereservation/performance/ReservationRepository.java
+++ src/main/java/com/numble/performancereservation/performance/ReservationRepository.java
@@ -1,8 +1,17 @@
 package com.numble.performancereservation.performance;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     long countByPerformanceId(Long performanceId);
+
+    @Query(value = "select case when count(*) > 0 then true else false end from reservation r "
+        + "join reservation_seat rs on r.id = rs.reservation_id "
+        + "where r.performance_id = :performanceId and rs.seat_id in :seatIds limit 1", nativeQuery = true)
+    boolean existsByReservationAndSeatIdIn(@Param("performanceId") Long performanceId,
+        @Param("seatIds") List<Long> seatIds);
 }
