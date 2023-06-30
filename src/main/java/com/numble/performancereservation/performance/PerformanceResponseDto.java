566928a9c96f4b43adfb0b505af7ccc91807c036
baeseohyeon <formnarago@naver.com>
2023-06-30 15:40:07 +0900

    FEAT: PerformanceResponseDto 날짜 타입 필드 직렬화, 역직렬화 어노테이션 추가

--- src/main/java/com/numble/performancereservation/performance/PerformanceResponseDto.java
+++ src/main/java/com/numble/performancereservation/performance/PerformanceResponseDto.java
@@ -1,5 +1,9 @@
 package com.numble.performancereservation.performance;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +14,11 @@ public class PerformanceResponseDto {
 
     private Long performanceId;
     private String title;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime startTime;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime endTime;
     private String venueName;
     private int normalPrice;
